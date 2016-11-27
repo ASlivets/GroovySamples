3deac3077a722a900df42e43ec51015ce0571f4c
aslivets <slivets.aleksay@gmail.com>
2016-11-27 22:49:37 +0100

    add WebServer

--- src/RESTClient.groovy
+++ src/RESTClient.groovy
@@ -1,7 +1,7 @@
 @Grab('org.codehaus.groovy.modules:http-builder-ng:0.10.2')
 
 def httpBin = groovyx.net.http.HttpBuilder.configure {
-    request.uri = 'http://localhost:8080/'
-}.get{}
+    request.uri = 'http://localhost:8080/s'
+}.get{path:'simple.json'}
 
 println (new String(httpBin as byte[]))
